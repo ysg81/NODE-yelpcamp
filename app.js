d092ef4ceb8b9a6399edae99afeba67822198a28
YSG <qpple6@naver.com>
2021-07-08 13:46:21 +0900

    CRUD 구현

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const path = require('path')
+const methodOverride = require('method-override')
 const mongoose = require('mongoose')
 const Campground = require('./models/campground')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
   useNewUrlParser: true,
   useCreateIndex: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  useFindAndModify: false
 })
 
 const db = mongoose.connection;
@@ -20,6 +22,8 @@ const app = express()
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
   res.render('home')
@@ -30,9 +34,36 @@ app.get('/campgrounds', async(req, res) => {
   res.render('campgrounds/index', {campgrounds})
 })
 
+app.get('/campgrounds/new', (req, res) => {
+  res.render('campgrounds/new')
+})
+
+app.post('/campgrounds', async(req, res) => {
+  const newcampground = new Campground(req.body.campground)
+  await newcampground.save();
+  res.redirect(`/campgrounds/${newcampground._id}`)
+})
+
 app.get('/campgrounds/:id', async(req, res) => {
   const campground = await Campground.findById(req.params.id)
   res.render('campgrounds/show', {campground})
+})
+
+app.get('/campgrounds/:id/edit', async(req, res) => {
+  const campground = await Campground.findById(req.params.id)
+  res.render('campgrounds/edit', {campground})
+})
+
+app.put('/campgrounds/:id', async(req, res) => {
+  const {id} = req.params
+  const editcampground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
+  res.redirect(`/campgrounds/${editcampground._id}`)
+})
+
+app.delete('/campgrounds/:id', async(req, res) => {
+  const {id} = req.params
+  await Campground.findByIdAndDelete(id)
+  res.redirect('/campgrounds')
 })
 
 app.listen(3000, () => {
