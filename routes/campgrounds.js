3eadad65e9036157be3351821d2eefa77072bef1
YSG <qpple6@naver.com>
2021-07-14 00:02:12 +0900

    Router 분리

---
+++ routes/campgrounds.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const router = express.Router()
+
+// error catch
+const catchAsync = require('../utils/catchAsync')
+const ExpressError = require('../utils/ExpressError')
+
+// define model
+const Campground = require('../models/campground')
+const {campgroundSchema, reviewSchema} = require('../joiSchemas')
+
+// Joi 사용법 - campground
+const validateCampground = (req, res, next) => {    
+  console.log(req.body)
+  const {error} = campgroundSchema.validate(req.body);
+  if(error){
+    // detail의 개수가 1개 이상일 경우
+    const msg = error.details.map(el => el.message).join(',');
+    throw new ExpressError(msg, 400)
+  } else{
+    next()
+  }
+}
+
+router.get('/', catchAsync(async(req, res) => {
+  const campgrounds = await Campground.find({})
+  res.render('campgrounds/index', {campgrounds})
+}))
+
+router.get('/new', (req, res) => {
+  res.render('campgrounds/new')
+})
+
+router.post('/', validateCampground, catchAsync(async (req, res, next) => {
+    const newcampground = new Campground(req.body.campground)
+    await newcampground.save();
+    res.redirect(`/campgrounds/${newcampground._id}`)
+}))
+
+router.get('/:id', catchAsync(async(req, res) => {
+  const a_campground = await Campground.findById(req.params.id).populate('reviews')
+  console.log(a_campground)
+  res.render('campgrounds/show', {a_campground})
+}))
+
+router.get('/:id/edit', catchAsync(async(req, res) => {
+  const editcampground = await Campground.findById(req.params.id)
+  res.render('campgrounds/edit', {editcampground})
+}))
+
+router.put('/:id', catchAsync(async(req, res) => {
+  const {id} = req.params
+  const editcampground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
+  res.redirect(`/campgrounds/${editcampground._id}`)
+}))
+
+router.delete('/:id', catchAsync(async(req, res) => {
+  const {id} = req.params
+  await Campground.findByIdAndDelete(id)
+  res.redirect('/campgrounds')
+}))
+
+
+module.exports = router
---
+++ routes/reviews.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const router = express.Router({mergeParams: true})
+// req.params값을 유지하도록 하는 것
+
+// error catch
+const catchAsync = require('../utils/catchAsync')
+const ExpressError = require('../utils/ExpressError')
+
+// define model
+const Campground = require('../models/campground')
+const Review = require('../models/review')
+const {campgroundSchema, reviewSchema} = require('../joiSchemas')
+
+// Joi 사용법 - review
+const validateRivew = (req, res, next) => {
+  console.log(req.body)
+  const {error} = reviewSchema.validate(req.body)
+  if(error){
+    const msg = error.details.map(el => el.message).join(',');
+    throw new ExpressError(msg, 400)
+  } else{
+    next()
+  }
+}
+
+router.post('/', validateRivew, catchAsync(async(req, res, next) => {
+  // 1. campground 모델을 찾음
+  const campground = await Campground.findById(req.params.id);
+  // 2. review 모델을 생성함
+  const review = new Review(req.body.review)
+  // 3. review 모델을 campground모델에 push
+  campground.reviews.push(review)
+  // 4. 저장
+  await review.save()
+  await campground.save()
+  res.redirect(`/campgrounds/${campground._id}`)
+}))
+
+router.delete('/:reviewId', catchAsync(async(req, res) => {
+  const {id, reviewId} = req.params;
+  /********not*********/
+  await Review.findByIdAndDelete(reviewId);
+  await Campground.findByIdAndUpdate(id, {$pull: { reviews: reviewId }})
+  /********not*********/
+  res.redirect(`/campgrounds/${id}`)
+}))
+
+
+module.exports = router
