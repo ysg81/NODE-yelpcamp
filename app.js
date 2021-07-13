e013d736c9208a9800aac4b57abb0aabd0cb3255
YSG <qpple6@naver.com>
2021-07-13 17:57:58 +0900

    Review create 및 validation 추가

--- app.js
+++ app.js
@@ -9,13 +9,14 @@ const path = require('path')
 const methodOverride = require('method-override')
 const ejsMate = require('ejs-mate')
 const Joi = require('joi')
-const {campgroundSchema} = require('./joiSchemas')
+const {campgroundSchema, reviewSchema} = require('./joiSchemas')
 
 const catchAsync = require('./utils/catchAsync')
 const ExpressError = require('./utils/ExpressError')
 
 const mongoose = require('mongoose')
 const Campground = require('./models/campground')
+const Review = require('./models/review')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
   useNewUrlParser: true,
@@ -41,7 +42,7 @@ app.use(methodOverride('_method'))
 
 
 
-// Joi 사용법
+// Joi 사용법 - campground
 const validateCampground = (req, res, next) => {    
   console.log(req.body)
   const {error} = campgroundSchema.validate(req.body);
@@ -54,6 +55,17 @@ const validateCampground = (req, res, next) => {
   }
 }
 
+// Joi 사용법 - review
+const validateRivew = (req, res, next) => {
+  console.log(req.body)
+  const {error} = reviewSchema.validate(req.body)
+  if(error){
+    const msg = error.details.map(el => el.message).join(',');
+    throw new ExpressError(msg, 400)
+  } else{
+    next()
+  }
+}
 
 
 app.get('/', (req, res) => {
@@ -69,7 +81,7 @@ app.get('/campgrounds/new', (req, res) => {
   res.render('campgrounds/new')
 })
 
-app.post('/campgrounds', validateCampground,  catchAsync(async (req, res, next) => {
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
     const newcampground = new Campground(req.body.campground)
     await newcampground.save();
     res.redirect(`/campgrounds/${newcampground._id}`)
@@ -85,7 +97,7 @@ app.get('/campgrounds/:id/edit', catchAsync(async(req, res) => {
   res.render('campgrounds/edit', {editcampground})
 }))
 
-app.put('/campgrounds/:id', validateCampground, catchAsync(async(req, res) => {
+app.put('/campgrounds/:id', catchAsync(async(req, res) => {
   const {id} = req.params
   const editcampground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
   res.redirect(`/campgrounds/${editcampground._id}`)
@@ -96,6 +108,20 @@ app.delete('/campgrounds/:id', catchAsync(async(req, res) => {
   await Campground.findByIdAndDelete(id)
   res.redirect('/campgrounds')
 }))
+
+app.post('/campgrounds/:id/reviews', validateRivew, catchAsync(async(req, res, next) => {
+  // 1. campground 모델을 찾음
+  const campground = await Campground.findById(req.params.id);
+  // 2. review 모델을 생성함
+  const review = new Review(req.body.review)
+  // 3. review 모델을 campground모델에 push
+  campground.reviews.push(review)
+  // 4. 저장
+  await review.save()
+  await campground.save()
+  res.redirect(`/campgrounds/${campground._id}`)
+}))
+
 
 // 모든 path를 통과하고, 주어진 모든 err를 건너뛰고 err가 발생할 경우
 app.all('*', (req, res, next) => {
